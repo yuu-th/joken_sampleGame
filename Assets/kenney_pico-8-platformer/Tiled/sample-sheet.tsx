49aad74676d22ca9e0defa1dd1fd09163ee9f7c9
maguro256 <1030seima1030@gmail.com>
2025-03-06 15:59:32 +0900

    2025/03/06

---
+++ Assets/kenney_pico-8-platformer/Tiled/sample-sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tilemap_packed" tilewidth="8" tileheight="8" tilecount="150" columns="15">
+ <image source="../Default/Tilemap/tilemap_packed.png" width="120" height="80"/>
+</tileset>
